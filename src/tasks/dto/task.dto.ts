ad9086b2663f42d87ab16a9679a1c8c3594891dd
AgusPostai <agustin.postai@gmail.com>
2024-01-25 12:21:44 -0300

    finalCommit

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,33 @@
 import {TaskStatus} from "../task.entity"
+import {
+    IsNotEmpty,
+    IsString,
+    MinLength,
+    IsOptional,
+    IsIn
+} from  "class-validator"
 
 export class CreateTaskDto {
-    title: string
+    @IsString()
+    @IsNotEmpty()
+    @MinLength(3)
+    title: string;
+
+    @IsString()
     description: string
 }
 
 export class UpdateTaskDto {
+    @IsString()
+    @IsOptional()
     title ?: string
+
+    @IsString()
+    @IsOptional()
     description?: string
+
+    @IsString()
+    @IsOptional()
+    @IsIn([TaskStatus.PENDING, TaskStatus.DONE, TaskStatus.IN_PROGRESS])
     status?: TaskStatus
 }
